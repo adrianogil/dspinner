a423a4d3de80da4f4dafc7ea4d4fa72780f3814b
Adriano Gil <adrianogil.san@gmail.com>
2018-10-16 20:33:01 -0400

    Initial commit

---
+++ DroidSpinner/Assets/Scripts/Spinner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour {
+
+    private Camera camera;
+
+    [Range(0f,1f)]
+    public float fakeLerpFactor;
+
+    public Color colorA, colorB;
+
+	// Use this for initialization
+	void Start () {
+		
+        camera = Camera.main;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        #if UNITY_EDITOR
+        camera.backgroundColor = Color.Lerp(colorA, colorB, fakeLerpFactor);
+        #elif UNITY_ANDROID
+        fakeLerpFactor = Mathf.Abs(0.5f*Input.acceleration.y);
+        camera.backgroundColor = Color.Lerp(colorA, colorB, fakeLerpFactor);
+        #endif
+	}
+}
